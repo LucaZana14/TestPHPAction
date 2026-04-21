5d166821febf491a21cbf266f83fe1059a7699d5
LucaZana14 <luczanardi02@gmail.com>
2026-04-21 10:02:35 +0200

    update github tainted id viewsource

--- vulnerabilities/view_source_all.php
+++ vulnerabilities/view_source_all.php
@@ -11,6 +11,10 @@ $page[ 'title' ] = 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
 if (array_key_exists ("id", $_GET)) {
 	$id = $_GET[ 'id' ];
 
+	if (!preg_match('/^[a-zA-Z0-9_-]+$/', $id)) {
+        die("Security Alert: Invalid ID format.");
+    }
+
 	$lowsrc = @file_get_contents("./{$id}/source/low.php");
 	$lowsrc = str_replace( array( '$html .=' ), array( 'echo' ), $lowsrc);
 	$lowsrc = highlight_string( $lowsrc, true );
